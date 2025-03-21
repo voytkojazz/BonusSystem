13bfd7d13e100bc54dff74cb50eaadd05c9fbce8
Rostyslav Voitko <75126304+voytkojazz@users.noreply.github.com>
2025-03-21 11:17:06 +0100

    adding NormalEmployee.java

---
+++ src/main/java/ua/rostyslav/NormalEmployee.java
@@ -0,0 +1,20 @@
+package ua.rostyslav;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class NormalEmployee implements Employee {
+
+    private final String name;
+    private final int yearsAtCompany;
+    private final int performanceScore;
+    private final int completedProjects;
+    private final int absenceDays;
+
+    @Override
+    public boolean isTeamLeader() {
+        return false;
+    }
+}
