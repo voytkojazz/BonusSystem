bb42ae4563ba0c6ea62a6b9877e2b96ca0f855ca
Rostyslav Voitko <75126304+voytkojazz@users.noreply.github.com>
2025-03-21 11:10:32 +0100

    adding Employee.java

---
+++ src/main/java/ua/rostyslav/Employee.java
@@ -0,0 +1,15 @@
+package ua.rostyslav;
+
+public interface Employee {
+    String getName();
+
+    int getYearsAtCompany();
+
+    int getPerformanceScore();
+
+    int getCompletedProjects();
+
+    int getAbsenceDays();
+
+    boolean isTeamLeader();
+}
