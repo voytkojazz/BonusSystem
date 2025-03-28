f44ab50835c9e3e33f8d55fbac38a6b618ae994c
Mohamad Suleiman <mohamed.suleiman@hotmail.de>
2025-03-28 10:39:08 +0100

    LowAbsenceBonus class + HighAbsencePenalty

---
+++ src/main/java/de/szut/HighAbsencePenalty.java
@@ -0,0 +1,24 @@
+package de.szut;
+
+public class HighAbsencePenalty extends BonusDecorator {
+        private int firstThreshold;
+        private int secondThreshold;
+        private double firstLevelPenaltyFactor;
+        private double secondLevelPenaltyFactor;
+
+        public HighAbsencePenalty(Bonus decoratedBonus, int firstThreshold, int secondThreshold,
+                                  double firstLevelPenaltyFactor, double secondLevelPenaltyFactor) {
+                super(decoratedBonus);
+                this.firstThreshold = firstThreshold;
+                this.secondThreshold = secondThreshold;
+                this.firstLevelPenaltyFactor = firstLevelPenaltyFactor;
+                this.secondLevelPenaltyFactor = secondLevelPenaltyFactor;
+        }
+
+        @Override
+        public double calculate(Employee employee, double currentBonus) {
+                return 0;
+        }
+
+
+}
---
+++ src/main/java/de/szut/LowAbsenceBonus.java
@@ -0,0 +1,24 @@
+package de.szut;
+
+public class LowAbsenceBonus extends BonusDecorator {
+    private int firstThreshold;
+    private int secondThreshold;
+    private double firstLevelBonus;
+    private double secondLevelBonus;
+    private Bonus decoratedBonus;
+
+    public LowAbsenceBonus(Bonus decoratedBonus, int firstThreshold, int secondThreshold, double firstLevelBonus, double secondLevelBonus) {
+        super(decoratedBonus);
+        this.decoratedBonus = decoratedBonus;
+        this.firstThreshold = firstThreshold;
+        this.secondThreshold = secondThreshold;
+        this.firstLevelBonus = firstLevelBonus;
+        this.secondLevelBonus = secondLevelBonus;
+    }
+
+
+    @Override
+    public double calculate(Employee employee, double currentBonus) {
+        return 0;
+    }
+}
